23823d88463febaf32ed7989673c581b737612a9
tree0122 <cuiyaohua11@163.com>
2019-01-28 10:54:49 +0800

    ribbon consumer

--- ribbon-consumer/src/main/java/com/tree/spring/cloud/action/ribbonconsumer/web/ConsumerController.java
+++ ribbon-consumer/src/main/java/com/tree/spring/cloud/action/ribbonconsumer/web/ConsumerController.java
@@ -1,4 +1,18 @@
 package com.tree.spring.cloud.action.ribbonconsumer.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class ConsumerController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/ribbon-consumer")
+    public String helloConsumer(){
+        return restTemplate.getForEntity("http://eureka-hello/hello", String.class).getBody();
+    }
 }
